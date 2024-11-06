9627f3ae514728bb03744f9f120dd57360a478a0
mzpsh <mirzamiftahulfadillah@gmail.com>
2024-11-06 09:28:17 +0800

    hotfix frekuensi button

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -68,14 +68,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2b6b2006fadbab561ac3b8f1e09b71e2
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "c07414a0d667ee9504818f79a1dc2c3b",
+"flutter_bootstrap.js": "e89fbf159bcbff5b5f82730bc26dd948",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "bb4b62bfa21eb3dd0a12b261e2ef50aa",
 "/": "bb4b62bfa21eb3dd0a12b261e2ef50aa",
-"main.dart.js": "743e5d21751df857d2dcb9efa683370f",
+"main.dart.js": "7c8419822ce237271bbfa362e4cd5560",
 "manifest.json": "97d6eee781803cc070e38a92ce60b4ea",
 "version.json": "2fcf1fbf413af478b2f267f9604937d7"};
 // The application shell files that are downloaded before a service worker can
