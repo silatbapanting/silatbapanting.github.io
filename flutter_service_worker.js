d6f5595d6a6b00a38cb6d7e45c3feba24982dba2
mzpsh <mirzamiftahulfadillah@gmail.com>
2024-11-10 21:38:40 +0800

    add subrecord, fix semesta form

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -92,14 +92,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e789d4efa35d5cd13c810933ed393397
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "126b41bfa4e6eab0bd8afd1f3f25aea9",
+"flutter_bootstrap.js": "9aeb2903643b3a7d75d50834a5c73fe0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "bb4b62bfa21eb3dd0a12b261e2ef50aa",
 "/": "bb4b62bfa21eb3dd0a12b261e2ef50aa",
-"main.dart.js": "731e759506cd03a7434838234bee038d",
+"main.dart.js": "dc6a789d7497124a286fdb35e0cae828",
 "manifest.json": "97d6eee781803cc070e38a92ce60b4ea",
 "version.json": "2fcf1fbf413af478b2f267f9604937d7"};
 // The application shell files that are downloaded before a service worker can
