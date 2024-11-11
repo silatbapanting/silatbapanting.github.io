da9db7fe93513ee18f80c211d2c9bd8d5dd63d5a
mzpsh <mirzamiftahulfadillah@gmail.com>
2024-11-11 19:06:18 +0800

    hotfix semesta

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1151843125"
+    serviceWorkerVersion: "1211196670"
   }
 });
