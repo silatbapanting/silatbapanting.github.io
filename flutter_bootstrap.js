0992cf0a11b9dd09c099e132f3b6e5a6b9c3df22
mzpsh <>
2024-11-30 15:12:57 +0800

    updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2422982521"
+    serviceWorkerVersion: "3329599078"
   }
 });
